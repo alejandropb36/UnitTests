e4d582479387a5392bf9047c4c940215863a505e
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-09 13:10:59 -0500

    feat: metodo de test

---
+++ Libreria.MSTests/OperacionMSTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Libreria.MSTests
+{
+    [TestClass]
+    public class OperacionMSTests
+    {
+        [TestMethod]
+        public void SumaNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            // Arrange
+            // Inicializacion de los valores que se necesitan
+            Operacion operacion = new();
+            int a = 50;
+            int b = 69;
+
+            // Act
+            int resultado = operacion.SumarNumeros(a, b);
+
+            // Assert
+            Assert.AreEqual(119, resultado);
+        }
+    }
+}
