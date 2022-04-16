0033c7c6f5cbd99bae5213c93a034ab26381389a
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-15 19:22:37 -0500

    feat: usando cadenas de texto

---
+++ Libreria.NUnitTests/ClienteNUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Libreria
+{
+    [TestFixture]
+    public class ClienteNUnitTests
+    {
+        [Test]
+        public void CrearNombreCompleto_InputNombreApellido_ReturnNombreCompleto()
+        {
+            // Arrange
+            Cliente cliente = new();
+
+            // Act
+            string nombreCompleto = cliente.CrearNombreCompleto("Alejandro", "Ponce");
+
+            // Assert
+            Assert.That(nombreCompleto, Is.EqualTo("Alejandro Ponce"));
+        }
+    }
+}
---
+++ Libreria/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace Libreria
+{
+    public class Cliente
+    {
+        public string CrearNombreCompleto(string nombre, string apellido)
+        {
+            return $"{nombre} {apellido}";
+        }
+    }
+}
