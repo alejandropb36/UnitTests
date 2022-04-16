b4fc196f5f89cbc389253391db324ebb61c21c81
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-15 20:10:54 -0500

    feat: setup de clase test

--- Libreria.NUnitTests/ClienteNUnitTests.cs
+++ Libreria.NUnitTests/ClienteNUnitTests.cs
@@ -5,17 +5,33 @@ namespace Libreria
     [TestFixture]
     public class ClienteNUnitTests
     {
+        private Cliente cliente;
+
+        [SetUp]
+        public void SetUp()
+        {
+            cliente = new Cliente();
+        }
+
         [Test]
         public void CrearNombreCompleto_InputNombreApellido_ReturnNombreCompleto()
         {
-            // Arrange
-            Cliente cliente = new();
-
             // Act
-            string nombreCompleto = cliente.CrearNombreCompleto("Alejandro", "Ponce");
+            cliente.CrearNombreCompleto("Alejandro", "Ponce");
 
             // Assert
-            Assert.That(nombreCompleto, Is.EqualTo("Alejandro Ponce"));
+            Assert.That(cliente.NombreCliente, Is.EqualTo("Alejandro Ponce"));
+            Assert.AreEqual(cliente.NombreCliente, "Alejandro Ponce");
+            Assert.That(cliente.NombreCliente, Does.Contain("Ponce"));
+            Assert.That(cliente.NombreCliente, Does.Contain("ponce").IgnoreCase);
+            Assert.That(cliente.NombreCliente, Does.StartWith("alejandro").IgnoreCase);
+            Assert.That(cliente.NombreCliente, Does.EndWith("ponce").IgnoreCase);
+        }
+
+        [Test]
+        public void NombreCliente_NoValues_ReturnNull()
+        {
+            Assert.IsNull(cliente.NombreCliente);
         }
     }
 }
--- Libreria/Cliente.cs
+++ Libreria/Cliente.cs
@@ -2,9 +2,12 @@
 {
     public class Cliente
     {
+        public string? NombreCliente { get; set; }
+
         public string CrearNombreCompleto(string nombre, string apellido)
         {
-            return $"{nombre} {apellido}";
+            NombreCliente = $"{nombre} {apellido}";
+            return NombreCliente;
         }
     }
 }
