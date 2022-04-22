21a256d38c6be719e8307f2258361431723a789e
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-21 21:42:11 -0500

    feat: caso de uso cuenta bancaria

---
+++ Libreria/CuentaBancaria.cs
@@ -0,0 +1,34 @@
+﻿namespace Libreria
+{
+    public class CuentaBancaria
+    {
+        private int _balance;
+
+        public CuentaBancaria()
+        {
+            _balance = 0;
+        }
+
+        public bool Deposito(int monto)
+        {
+            _balance += monto;
+            return true;
+        }
+
+        public bool Retiro(int monto)
+        {
+            if (monto <= _balance)
+            {
+                _balance -= monto;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
