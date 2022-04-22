72fdd03fab6c90285eb2890fc6ddce7a6198a1e8
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-21 21:49:45 -0500

    feat: logger para caso de uso

--- Libreria/CuentaBancaria.cs
+++ Libreria/CuentaBancaria.cs
@@ -3,15 +3,18 @@
     public class CuentaBancaria
     {
         private int _balance;
+        private readonly ILoggerGeneral _loggerGeneral;
 
-        public CuentaBancaria()
+        public CuentaBancaria(ILoggerGeneral loggerGeneral)
         {
             _balance = 0;
+            _loggerGeneral = loggerGeneral;
         }
 
         public bool Deposito(int monto)
         {
             _balance += monto;
+            _loggerGeneral.Message($"El monto: {monto} fue depositado correctamente, balance: {_balance}");
             return true;
         }
 
@@ -20,14 +23,17 @@
             if (monto <= _balance)
             {
                 _balance -= monto;
+                _loggerGeneral.Message($"Retiro exitoso por el monto: {monto}, balance restante: {_balance}");
                 return true;
             }
 
+            _loggerGeneral.Message($"Error en el retiro, el monto: {monto} es superior al balance: {_balance}");
             return false;
         }
 
         public int GetBalance()
         {
+            _loggerGeneral.Message($"Consulta de balance: {_balance}");
             return _balance;
         }
     }
---
+++ Libreria/LoggerGeneral.cs
@@ -0,0 +1,15 @@
+﻿namespace Libreria
+{
+    public interface ILoggerGeneral
+    {
+        void Message(string message);
+    }
+
+    public class LoggerGeneral : ILoggerGeneral
+    {
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
