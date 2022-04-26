c280926e237e313677fef5399b0eb9a9711ec177
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-25 21:15:56 -0500

    feat: implementando mocking

--- Libreria.NUnitTests/CuentaBancariaNUnitTests.cs
+++ Libreria.NUnitTests/CuentaBancariaNUnitTests.cs
@@ -1,21 +1,34 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 
 namespace Libreria
 {
     [TestFixture]
     public class CuentaBancariaNUnitTests
     {
-        private CuentaBancaria cuentaBancaria;
+        private CuentaBancaria cuenta;
 
         [SetUp]
         public void SetUp()
         {
-            cuentaBancaria = new CuentaBancaria(new LoggerFake());
         }
 
         [Test]
-        public void Deposito_InputMonto100_ReturnTrue()
+        public void Deposito_InputMonto100LoggerFake_ReturnTrue()
         {
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(new LoggerFake());
+            int monto = 100;
+            var resultado = cuentaBancaria.Deposito(monto);
+
+            Assert.IsTrue(resultado);
+            Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(monto));
+        }
+
+        [Test]
+        public void Deposito_InputMonto100Mocking_ReturnTrue()
+        {
+            var LoggerMocking = new Mock<ILoggerGeneral>();
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(LoggerMocking.Object);
             int monto = 100;
             var resultado = cuentaBancaria.Deposito(monto);
 
