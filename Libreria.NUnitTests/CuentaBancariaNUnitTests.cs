b42eec0541e7a51f3459b400709fea9fd0a3cd8c
Alejandro Ponce <alexponce36@hotmail.com>
2022-04-27 21:24:54 -0500

    feat: mock con deafault values

--- Libreria.NUnitTests/CuentaBancariaNUnitTests.cs
+++ Libreria.NUnitTests/CuentaBancariaNUnitTests.cs
@@ -35,5 +35,42 @@ namespace Libreria
             Assert.IsTrue(resultado);
             Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(monto));
         }
+
+        [Test]
+        [TestCase(200, 100)]
+        [TestCase(200, 150)]
+        public void Retiro_Retiro100ConBalance200_ReturnsTrue(int balance, int retiro)
+        {
+            // Arrange
+            var loggerMocking = new Mock<ILoggerGeneral>();
+            loggerMocking.Setup(l => l.LogDatabase(It.IsAny<string>())).Returns(true);
+            loggerMocking.Setup(l => l.LogBalanceDespuesRetiro(It.Is<int>(x => x > 0))).Returns(true);
+            CuentaBancaria cuenta = new(loggerMocking.Object);
+            cuenta.Deposito(balance);
+
+            // Act
+            var result = cuenta.Retiro(retiro);
+
+            // Asserts
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase(200, 300)]
+        public void Retiro_Retiro300ConBalance200_ReturnsFalse(int balance, int retiro)
+        {
+            // Arrange
+            var loggerMocking = new Mock<ILoggerGeneral>();
+            //loggerMocking.Setup(l => l.LogBalanceDespuesRetiro(It.Is<int>(x => x < 0))).Returns(false);
+            loggerMocking.Setup(l => l.LogBalanceDespuesRetiro(It.IsInRange<int>(int.MinValue, -1, Moq.Range.Inclusive))).Returns(false);
+            CuentaBancaria cuenta = new(loggerMocking.Object);
+            cuenta.Deposito(balance);
+
+            // Act
+            var result = cuenta.Retiro(retiro);
+
+            // Asserts
+            Assert.IsFalse(result);
+        }
     }
 }
