3b2fc6d4915bbf228d6a204fbf24e4425e7ccdcd
Alejandro Ponce <alexponce36@hotmail.com>
2022-05-05 20:43:48 -0500

    feat: pruebas cliente migradas a xunit

--- Libreria.XUnitTests/ClienteXUnitTests.cs
+++ Libreria.XUnitTests/ClienteXUnitTests.cs
@@ -1,97 +1,81 @@
-﻿//using NUnit.Framework;
+﻿using Xunit;
 
-//namespace Libreria
-//{
-//    [TestFixture]
-//    public class ClienteXUnitTests
-//    {
-//        private Cliente cliente;
+namespace Libreria
+{
+    public class ClienteXUnitTests
+    {
+        private Cliente cliente;
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            cliente = new Cliente();
-//        }
+        public ClienteXUnitTests()
+        {
+            cliente = new Cliente();
+        }
 
-//        [Test]
-//        public void CrearNombreCompleto_InputNombreApellido_ReturnNombreCompleto()
-//        {
-//            // Act
-//            cliente.CrearNombreCompleto("Alejandro", "Ponce");
+        [Fact]
+        public void CrearNombreCompleto_InputNombreApellido_ReturnNombreCompleto()
+        {
+            // Act
+            cliente.CrearNombreCompleto("Alejandro", "Ponce");
 
-//            // Assert
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(cliente.NombreCliente, Is.EqualTo("Alejandro Ponce"));
-//                Assert.AreEqual(cliente.NombreCliente, "Alejandro Ponce");
-//                Assert.That(cliente.NombreCliente, Does.Contain("Ponce"));
-//                Assert.That(cliente.NombreCliente, Does.Contain("ponce").IgnoreCase);
-//                Assert.That(cliente.NombreCliente, Does.StartWith("alejandro").IgnoreCase);
-//                Assert.That(cliente.NombreCliente, Does.EndWith("ponce").IgnoreCase);
-//            });
-//        }
+            // Assert
+            Assert.Equal("Alejandro Ponce", cliente.NombreCliente);
+            Assert.Contains("Ponce",cliente.NombreCliente);
+            Assert.StartsWith("Alejandro", cliente.NombreCliente);
+            Assert.EndsWith("Ponce", cliente.NombreCliente);
+        }
 
-//        [Test]
-//        public void NombreCliente_NoValues_ReturnNull()
-//        {
-//            Assert.IsNull(cliente.NombreCliente);
-//        }
+        [Fact]
+        public void NombreCliente_NoValues_ReturnNull()
+        {
+            Assert.Null(cliente.NombreCliente);
+        }
 
-//        [Test]
-//        public void DescuentoEvaluacion_DefaultCliente_ReturnsDecuentoIntervalo()
-//        {
-//            int descuento = cliente.Descuento;
+        [Fact]
+        public void DescuentoEvaluacion_DefaultCliente_ReturnsDecuentoIntervalo()
+        {
+            int descuento = cliente.Descuento;
 
-//            Assert.That(descuento, Is.InRange(4, 24));
-//        }
+            Assert.InRange(descuento, 5, 24);
+        }
 
-//        [Test]
-//        public void CrearNombreCompleto_InputNombre_ReturnNotNull()
-//        {
-//            // Act
-//            cliente.CrearNombreCompleto("Vaxi", "");
+        [Fact]
+        public void CrearNombreCompleto_InputNombre_ReturnNotNull()
+        {
+            // Act
+            cliente.CrearNombreCompleto("Vaxi", "");
 
-//            Assert.IsNotNull(cliente.NombreCliente);
-//            Assert.IsFalse(string.IsNullOrEmpty(cliente.NombreCliente));
-//        }
+            Assert.NotNull(cliente.NombreCliente);
+            Assert.False(string.IsNullOrEmpty(cliente.NombreCliente));
+        }
 
-//        [Test]
-//        public void CrearNombreCompleto_InputNombreEnBlanco_ThrowException()
-//        {
-//            var exceptionDetalle = Assert.Throws<ArgumentException>(() => cliente.CrearNombreCompleto("", "Ponce"));
+        [Fact]
+        public void CrearNombreCompleto_InputNombreEnBlanco_ThrowException()
+        {
+            var exceptionDetalle = Assert.Throws<ArgumentException>(() => cliente.CrearNombreCompleto("", "Ponce"));
 
-//            Assert.AreEqual("El nombre esta en blanco", exceptionDetalle.Message);
+            Assert.Equal("El nombre esta en blanco", exceptionDetalle.Message);
 
-//            Assert.That(() => 
-//                cliente.CrearNombreCompleto("", ""),
-//                Throws.ArgumentException.With.Message.EqualTo("El nombre esta en blanco")
-//            );
+            Assert.Throws<ArgumentException>(() => cliente.CrearNombreCompleto("", "Ponce"));
+        }
 
-//            Assert.Throws<ArgumentException>(() => cliente.CrearNombreCompleto("", "Ponce"));
-//            Assert.That(() =>
-//                cliente.CrearNombreCompleto("", ""),
-//                Throws.ArgumentException
-//            );
-//        }
+        [Fact]
+        public void GetClienteDetalle_CrearClienteConMenos500TotalOrder_ReturnsClienteBasico()
+        {
+            cliente.OrderTotal = 150;
 
-//        [Test]
-//        public void GetClienteDetalle_CrearClienteConMenos500TotalOrder_ReturnsClienteBasico()
-//        {
-//            cliente.OrderTotal = 150;
+            var result = cliente.GetClienteDetalle();
 
-//            var result = cliente.GetClienteDetalle();
+            Assert.IsType<ClienteBasico>(result);
+        }
 
-//            Assert.That(result, Is.TypeOf<ClienteBasico>());
-//        }
+        [Fact]
+        public void GetClienteDetalle_CrearClienteConMas500TotalOrder_ReturnsClientePremium()
+        {
+            cliente.OrderTotal = 501;
 
-//        [Test]
-//        public void GetClienteDetalle_CrearClienteConMas500TotalOrder_ReturnsClientePremium()
-//        {
-//            cliente.OrderTotal = 501;
+            var result = cliente.GetClienteDetalle();
 
-//            var result = cliente.GetClienteDetalle();
-
-//            Assert.That(result, Is.TypeOf<ClientePremium>());
-//        }
-//    }
-//}
+            Assert.IsType<ClientePremium>(result);
+        }
+    }
+}
